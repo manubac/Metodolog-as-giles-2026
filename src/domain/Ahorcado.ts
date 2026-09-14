7514faf99b7406cc981d77e81cb150f39b76c59d
Nicolás Piccoli <nicopiccoli07@gmail.com>
2026-09-14 20:21:53 -0300

    GREEN: Ahorcado revela la letra adivinada cuando está presente

--- src/domain/Ahorcado.ts
+++ src/domain/Ahorcado.ts
@@ -1,15 +1,20 @@
 export class Ahorcado {
   private readonly palabra: string;
   private readonly vidas: number = 6;
+  private readonly letrasAdivinadas = new Set<string>();
 
   constructor(palabra: string) {
     this.palabra = palabra;
   }
 
+  adivinar(letra: string): void {
+    this.letrasAdivinadas.add(letra);
+  }
+
   palabraEnmascarada(): string {
     return this.palabra
       .split("")
-      .map(() => "_")
+      .map((letra) => (this.letrasAdivinadas.has(letra) ? letra : "_"))
       .join(" ");
   }
 
